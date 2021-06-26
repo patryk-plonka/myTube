098089c9cfb0d4b10d98b1ab485bed3117b61dac
Patryk <patryk.plonka@gmail.com>
2021-06-26 15:31:59 +0200

    added video simple streaming

--- Hello/index.js
+++ Hello/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
     res.send('Hello World');
+});
+app.get("/video", (req, res) => {
+    const path = "../media/file_example_MP4_480_1_5MG.mp4";
+    fs.stat(path, (err, stats) => {
+        if(err) {
+            console.error("An error occured - http 500");
+            res.sendStatus(500);
+            return;
+        }
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+        fs.createReadStream(path).pipe(res);
+    });
 });
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
